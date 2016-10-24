0ac4eca2affac860ea8c23bb0e8c60ccf6c931a4
Ben Marshall <me@benmarshall.me>
2016-10-24 12:27:54 -0500

    Renamed index file.

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en-US" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#" itemscope="itemscope" itemtype="http://schema.org/WebPage">
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Frontend v1.1.0</title>
+<meta name="description" content="A light-weight, skeleton frontend framework.">
+<link rel="canonical" href="https://github.com/bmarshall511/frontend">
+
+<link rel="shortcut icon" href="favicon.ico">
+
+<!-- Google+ -->
+<link rel="publisher" href="https://plus.google.com/u/0/+BenMarshall511">
+
+<!-- Open Graph -->
+<meta property="og:locale" content="en_US">
+<meta property="og:type" content="article">
+<meta property="og:title" content="Frontend">
+<meta property="og:description" content="A light-weight, skeleton frontend framework.">
+<meta property="og:url" content="https://github.com/bmarshall511/frontend">
+<meta property="og:site_name" content="Frontend">
+
+<!-- Facebook -->
+<meta property="article:publisher" content="FACEBOOK PAGE URL">
+<meta property="fb:app_id" content="">
+<meta property="og:image" content="">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="1200">
+
+<!-- Twitter -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:description" content="A light-weight, skeleton frontend framework.">
+<meta name="twitter:title" content="Frontend">
+<meta name="twitter:site" content="@bmarshall0511">
+<meta name="twitter:image" content="">
+<meta name="twitter:creator" content="@bmarshall0511">
+
+<link rel="stylesheet" href="assets/css/style.css">
+
+<!-- DEMO STYLES - Can be removed & deleted -->
+<link rel="stylesheet" href="assets/css/demo.css">
+
+</head>
+<body class="demo">
+<div class="demo__content align-center">
+  <h1>Frontend <span>v1.1.0</span></h1>
+  <p><em>A light-weight, skeleton frontend framework.</em></p>
+  <ul class="list-inline-block">
+    <li><a href="https://github.com/bmarshall511/frontend" class="icon-github-circled"></a>
+  </ul>
+</div>
+
+<!-- Requires jQuery -->
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+
+<!-- DEVELOPMENT - global.js -->
+<script src="assets/js/global.js"></script>
+
+<!-- PRODUCTION - global.js -->
+<!--<script src="assets/js/global.min.js"></script>-->
+</body>
+</html>
