f63c3cbcb09f6dd37f6ed92096d87104f19a55f0
Ben Marshall <bmarshall511@gmail.com>
2017-04-28 09:49:39 -0500

    Misc updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,46 @@
 /* eslint-disable */
-var gulp        = require('gulp'),
-    jshint      = require('gulp-jshint'),
-    stylish     = require('jshint-stylish'),
-    paths       = require('compass-options').dirs(),
-    rename      = require('gulp-rename'),
-    stripDebug  = require('gulp-strip-debug'),
-    uglify      = require('gulp-uglify'),
-    concat      = require('gulp-concat'),
-    compass     = require('gulp-compass'),
-    prefix      = require('gulp-autoprefixer'),
-    cleanCSS    = require('gulp-clean-css'),
-    imagemin    = require('gulp-imagemin'),
-    complexity  = require('gulp-complexity'),
-    replace     = require('gulp-replace'),
-    del         = require('del'),
-    scsslint    = require('gulp-scss-lint'),
-    eslint      = require('gulp-eslint'),
-    htmlmin     = require('gulp-htmlmin'),
-    uncss       = require('gulp-uncss');
+const gulp        = require( 'gulp' ),
+      jshint      = require( 'gulp-jshint' ),
+      stylish     = require( 'jshint-stylish' ),
+      paths       = require( 'compass-options' ).dirs(),
+      rename      = require( 'gulp-rename' ),
+      stripDebug  = require( 'gulp-strip-debug' ),
+      uglify      = require( 'gulp-uglify' ),
+      concat      = require( 'gulp-concat' ),
+      compass     = require( 'gulp-compass' ),
+      prefix      = require( 'gulp-autoprefixer' ),
+      cleanCSS    = require( 'gulp-clean-css' ),
+      imagemin    = require( 'gulp-imagemin' ),
+      complexity  = require( 'gulp-complexity' ),
+      replace     = require( 'gulp-replace' ),
+      del         = require( 'del' ),
+      scsslint    = require( 'gulp-scss-lint' ),
+      eslint      = require( 'gulp-eslint' ),
+      htmlmin     = require( 'gulp-htmlmin' ),
+      uncss       = require( 'gulp-uncss' ),
+      notify      = require( 'gulp-notify' ),
+      plumber     = require( 'gulp-plumber' ),
+      cache       = require( 'gulp-cached' );
 
+// Error Handler
+const _onError = function( err ) {
+  notify.onError({
+    title   : 'Error',
+    message : '<%=  error %>'
+  })( err );
+};
 
 // Config Variables
-var sourceJs      = 'src/js',
-    sourceImg     = 'src/img',
-    compassConfig = {
-      config_file: './config.rb',
-      css: paths.css,
-      sass: paths.sass,
-      bundle_exec: true,
-      time: true
-    },
-    globalJS     = [
-      // Config (optional)
-      //sourceJs + '/frontend/frontend.js',
-
-      // Services (optional)
-      //sourceJs + '/frontend/services/callback.js',
-
-      // Example controller
-      //sourceJs + '/frontend/controllers/example.js'
-    ];
+const sourceJs      = 'src/js',
+      sourceImg     = 'src/img',
+      compassConfig = {
+        config_file : './config.rb',
+        css         : paths.css,
+        sass        : paths.sass,
+        bundle_exec : true,
+        time        : true
+      },
+      globalJS     = [];
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -47,9 +48,12 @@ var sourceJs      = 'src/js',
 ////////////////////////////////////////////////////////////////////////////////
 gulp.task('lint', function() {
   return gulp.src([
-    sourceJs + '/**/*.js',
-    '!' + sourceJs + '/application/lib/*'
+    sourceJs + '/**/*.js'
   ])
+    .pipe(plumber({
+      errorHandler: _onError
+    }))
+    .pipe(cache('lint'))
     .pipe(jshint())
     .pipe(jshint.reporter(stylish))
     .pipe(eslint())
@@ -61,7 +65,15 @@ gulp.task('lint', function() {
 // JavaScript Complexity Task
 ////////////////////////////////////////////////////////////////////////////////
 gulp.task('jscomplexity', ['lint'], function() {
-  return gulp.src([ sourceJs + '/**/*.js', '!' + sourceJs + '/application/lib/*', '!' + sourceJs + '/frontend/lib/*' ])
+  return gulp.src([
+    sourceJs + '/**/*.js',
+    '!' + sourceJs + '/application/lib/*',
+    '!' + sourceJs + '/frontend/lib/*'
+  ])
+    .pipe(plumber({
+      errorHandler: _onError
+    }))
+    .pipe(cache('jscomplexity'))
     .pipe(complexity());
 });
 
@@ -72,6 +84,10 @@ gulp.task('jscomplexity', ['lint'], function() {
 gulp.task('scripts', ['jscomplexity'], function() {
   // global.js
   gulp.src(globalJS)
+    .pipe(plumber({
+      errorHandler: _onError
+    }))
+    .pipe(cache('scripts'))
     .pipe(concat('global.js'))
     .pipe(gulp.dest(paths.js))
     .pipe(rename('global.min.js'))
@@ -83,6 +99,10 @@ gulp.task('scripts', ['jscomplexity'], function() {
 gulp.task('scripts-dev', ['jscomplexity'], function() {
   // global.js
   gulp.src(globalJS)
+    .pipe(plumber({
+      errorHandler: _onError
+    }))
+    .pipe(cache('scripts-dev'))
     .pipe(concat('global.js'))
     .pipe(gulp.dest(paths.js))
     .pipe(rename('global.min.js'))
@@ -95,12 +115,20 @@ gulp.task('scripts-dev', ['jscomplexity'], function() {
 ////////////////////////////////////////////////////////////////////////////////
 gulp.task('htmlmin', function() {
   return gulp.src('src/**/*.html')
+    .pipe(plumber({
+      errorHandler: _onError
+    }))
+    .pipe(cache('htmlmin'))
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('htmlmin-dev', function() {
   return gulp.src('src/**/*.html')
+    .pipe(plumber({
+      errorHandler: _onError
+    }))
+    .pipe(cache('htmlmin-dev'))
     .pipe(htmlmin({collapseWhitespace: false}))
     .pipe(gulp.dest('dist'));
 });
@@ -111,6 +139,10 @@ gulp.task('htmlmin-dev', function() {
 ////////////////////////////////////////////////////////////////////////////////
 gulp.task('scss-lint', function() {
   return gulp.src(paths.sass + '/**/*.scss')
+    .pipe(plumber({
+      errorHandler: _onError
+    }))
+    .pipe(cache('scss-lint'))
     .pipe(scsslint());
 });
 
@@ -120,22 +152,22 @@ gulp.task('scss-lint', function() {
 ////////////////////////////////////////////////////////////////////////////////
 gulp.task('compass-dev', ['images', 'scss-lint'], function() {
   return gulp.src(paths.sass + '/**/*.scss')
+    .pipe(plumber({
+      errorHandler: _onError
+    }))
+    .pipe(cache('compass-dev'))
     .pipe(compass(compassConfig))
-    .on('error', function(error) {
-      console.log(error);
-      this.emit('end');
-    })
     .pipe(prefix(["last 1 version", "> 1%", "ie 9"]))
     .pipe(gulp.dest(paths.css));
 });
 
 gulp.task('compass', ['images', 'scss-lint'], function() {
   return gulp.src(paths.sass + '/**/*.scss')
+    .pipe(plumber({
+      errorHandler: _onError
+    }))
+    .pipe(cache('compass'))
     .pipe(compass(compassConfig))
-    .on('error', function(error) {
-      console.log(error);
-      this.emit('end');
-    })
     .pipe(prefix(["last 1 version", "> 1%", "ie 9"]))
     .pipe(cleanCSS({
       compatibility: 'ie9'
@@ -152,6 +184,10 @@ gulp.task('compass', ['images', 'scss-lint'], function() {
 ////////////////////////////////////////////////////////////////////////////////
 gulp.task('uncss', ['compass', 'htmlmin'], function() {
   return gulp.src('dist/css/**/*.css')
+    .pipe(plumber({
+      errorHandler: _onError
+    }))
+    .pipe(cache('uncss'))
     .pipe(uncss({
       html: ['src/**/*.html']
     }))
@@ -164,6 +200,10 @@ gulp.task('uncss', ['compass', 'htmlmin'], function() {
 ////////////////////////////////////////////////////////////////////////////////
 gulp.task('images', function() {
   return gulp.src(sourceImg + '/**/*')
+    .pipe(plumber({
+      errorHandler: _onError
+    }))
+    .pipe(cache('images'))
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
 });
@@ -173,8 +213,8 @@ gulp.task('images', function() {
 // Watch Task
 ////////////////////////////////////////////////////////////////////////////////
 gulp.task('watch', function () {
-  gulp.watch(sourceJs + '/**/*.js', ['scripts']);
-  gulp.watch(paths.sass + '/**/*.scss', ['compass']);
+  gulp.watch(sourceJs + '/**/*.js', ['scripts-dev']);
+  gulp.watch(paths.sass + '/**/*.scss', ['compass-dev']);
   gulp.watch(sourceImg + '/*', ['images']);
   gulp.watch('src/scss/frontend/global/_variables.scss', ['variables']);
   gulp.watch('src/**/*.html', ['htmlmin-dev']);
@@ -186,10 +226,17 @@ gulp.task('watch', function () {
 ////////////////////////////////////////////////////////////////////////////////
 gulp.task('variables', function() {
   gulp.src('src/scss/frontend/global/_variables-custom.scss')
+      .pipe(plumber({
+        errorHandler: _onError
+      }))
+      .pipe(cache('variables'))
       .pipe(replace('\n//\n// DO NOT EDIT! To add/override variables, create a _variables-custom.scss file\n// in src/scss/application/global.', ''))
       .pipe(gulp.dest('src/scss/application/global'));
 
   gulp.src('src/scss/frontend/global/_variables.scss')
+      .pipe(plumber({
+        errorHandler: _onError
+      }))
       .pipe(replace('Framework Variables', 'Application Variables'))
       .pipe(replace('All framework pre-packaged variables.', 'DO NOT DELETE. Should contain all application variables and Frontend variable\r\n// overrides. Clone src/scss/frontend/global/_variables.scss as a start.'))
       .pipe(replace('\n//\n// DO NOT EDIT! To add/override variables, create a _variables.scss file in\n// src/scss/application/global.', ''))
