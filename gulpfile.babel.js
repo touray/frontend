bfcbcaa9a99ed6288405d89a2ac3e0d191e21113
Ben Marshall <bmarshall511@gmail.com>
2017-06-21 12:07:51 -0500

    Misc updates

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,4 @@
-// Import required node modules
+// Gulp imports
 import cleanCSS from 'gulp-clean-css';
 import concat from 'gulp-concat';
 import compass from 'gulp-compass';
@@ -8,9 +8,7 @@ import gulp from 'gulp';
 import htmlmin from 'gulp-htmlmin';
 import ifElse from 'gulp-if-else';
 import imagemin from 'gulp-imagemin';
-import kss from 'kss';
 import notify from 'gulp-notify';
-import paths from 'compass-options';
 import plumber from 'gulp-plumber';
 import prefix from 'gulp-autoprefixer';
 import replace from 'gulp-replace';
@@ -18,31 +16,54 @@ import scsslint from 'gulp-scss-lint';
 import sourcemaps from 'gulp-sourcemaps';
 import stripDebug from 'gulp-strip-debug';
 import uglify from 'gulp-uglify';
+
+// Import required node modules
+import kss from 'kss';
+import paths from 'compass-options';
+import {argv} from 'yargs';
+
+// Including gulp-uncss here due to a bug including before kss
 import uncss from 'gulp-uncss';
 
-// Application configuration
-const cleanOptions  = {
-        compatibility: 'ie9'
-      },
-      compassConfig = {
-        config_file : './config.rb',
-        css         : paths.dirs().css,
-        sass        : paths.dirs().sass,
-        bundle_exec : true,
-        time        : true
-      },
-      cssPrefix     = ["last 1 version", "> 1%", "ie 9"],
-      defaultTasks  = ['uncss', 'js-transpile'],
-      distHTML      = 'dist',
-      distImg       = paths.dirs().img,
-      distJS        = paths.dirs().js,
-      srcHTML       = 'src/**/*.html',
-      srcImg        = 'src/img',
-      srcJS         = 'src/js',
-      stylesDir     = 'styleguide';
+// If gulp was called in the terminal with the --prod flag, set the node environment to production
+if (argv.prod) {
+  process.env.NODE_ENV = 'production';
+}
+const PROD = process.env.NODE_ENV === 'production';
+
+// Configuration
+const src = 'src';
+const config = {
+  clean: {
+    compatibility: 'ie9'
+  },
+  compass: {
+    config_file : './config.rb',
+    css         : paths.dirs().css,
+    sass        : paths.dirs().sass,
+    bundle_exec : true,
+    time        : true
+  },
+  default: ['uncss', 'js-transpile'],
+  kss: {
+    // Relative to src directory
+    css: ['../dist/css/style.css'],
+    homepage: 'README.md',
+    js: ['../dist/js/app.js'],
+    title: 'Frontend Build Styleguide'
+  },
+  paths: {
+    html: 'dist',
+    srcHtml: src + '/**/*.html',
+    srcImg: src + 'img/**/*',
+    srcJs: src + 'js/**/*.js',
+    styleguide: 'styleguide'
+  },
+  prefix: ["last 1 version", "> 1%", "ie 9"]
+};
 
 // app.js imports
-const appJS = [srcJS + '/app.js'];
+const appJs = [src + '/js/app.js'];
 
 // Error handling
 const _onError = (err) => {
@@ -58,10 +79,10 @@ gulp.task('compass', ['images', 'scss-lint'], () => {
     .pipe(plumber({
       errorHandler: _onError
     }))
-    .pipe(compass(compassConfig))
-    .pipe(prefix(cssPrefix))
-    .pipe(ifElse(process.env.NODE_ENV === 'production', function() {
-      return cleanCSS(cleanOptions);
+    .pipe(compass(config.compass))
+    .pipe(prefix(config.prefix))
+    .pipe(ifElse(PROD === 'production', function() {
+      return cleanCSS(config.clean);
     }))
     .pipe(gulp.dest(paths.dirs().css));
 });
@@ -69,35 +90,35 @@ gulp.task('compass', ['images', 'scss-lint'], () => {
 // Task: copy-readme
 gulp.task('copy-readme', () => {
   return gulp.src('README.md')
-    .pipe(gulp.dest('src/scss'));
+    .pipe(gulp.dest(paths.dirs().sass));
 });
 
 // Task: default
-gulp.task('default', defaultTasks);
+gulp.task('default', config.default);
 
 // Task: htmlmin
 gulp.task('htmlmin', () => {
-  return gulp.src(srcHTML)
+  return gulp.src(config.paths.srcHtml)
     .pipe(plumber({
       errorHandler: _onError
     }))
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest(distHTML));
+    .pipe(gulp.dest(config.paths.html));
 });
 
 // Task: images
 gulp.task('images', () => {
-  return gulp.src(srcImg + '/**/*')
+  return gulp.src(config.paths.srcImg)
     .pipe(plumber({
       errorHandler: _onError
     }))
     .pipe(imagemin())
-    .pipe(gulp.dest(distImg));
+    .pipe(gulp.dest(paths.dirs().img));
 });
 
 // Task: js-complexity
 gulp.task('js-complexity', ['js-lint'], () => {
-  return gulp.src(srcJS + '/**/*.js')
+  return gulp.src(config.paths.srcJs)
     .pipe(plumber({
       errorHandler: _onError
     }))
@@ -106,7 +127,7 @@ gulp.task('js-complexity', ['js-lint'], () => {
 
 // Task: js-lint
 gulp.task('js-lint', () => {
-  return gulp.src([srcJS + '/**/*.js', 'gulpfile.babel.js'])
+  return gulp.src([config.paths.srcJs, 'gulpfile.babel.js'])
     .pipe(plumber({
       errorHandler: _onError
     }))
@@ -116,27 +137,27 @@ gulp.task('js-lint', () => {
 
 // Task: js-transpile
 gulp.task('js-transpile', ['js-complexity'], () => {
-  gulp.src(appJS)
+  gulp.src(appJs)
     .pipe(plumber({
       errorHandler: _onError
     }))
     .pipe(sourcemaps.init())
     .pipe(concat('app.js'))
     .pipe(sourcemaps.write())
-    .pipe(ifElse(process.env.NODE_ENV === 'production', stripDebug))
-    .pipe(ifElse(process.env.NODE_ENV === 'production', uglify))
-    .pipe(gulp.dest(distJS));
+    .pipe(ifElse(PROD === 'production', stripDebug))
+    .pipe(ifElse(PROD === 'production', uglify))
+    .pipe(gulp.dest(paths.dirs().js));
 });
 
 // Task: kss
 gulp.task('kss', ['compass', 'copy-readme'], () => {
   return kss({
-    source: 'src/scss',
-    destination: stylesDir,
-    css: '../dist/css/style.css',
-    js: '../dist/js/app.js',
-    homepage: 'README.md',
-    title: 'Frontend Build Styleguide'
+    source: paths.dirs().sass,
+    destination: config.paths.styleguide,
+    css: config.kss.css,
+    js: config.kss.js,
+    homepage: config.kss.homepage,
+    title: config.kss.title
   });
 });
 
@@ -156,7 +177,7 @@ gulp.task('uncss', ['compass', 'htmlmin'], () => {
       errorHandler: _onError
     }))
     .pipe(uncss({
-      html: [srcHTML]
+      html: [config.paths.srcHtml]
     }))
     .pipe(gulp.dest(paths.dirs().css));
 });
@@ -182,9 +203,9 @@ gulp.task('variables', () => {
 // Task: watch
 gulp.task('watch', () => {
   gulp.watch(paths.dirs().sass + '/**/*.scss', ['kss']);
-  gulp.watch(srcImg + '/**/*', ['images']);
-  gulp.watch(srcJS + '/**/*.js', ['js-transpile']);
-  gulp.watch(srcHTML, ['htmlmin']);
+  gulp.watch(config.paths.srcImg, ['images']);
+  gulp.watch(config.paths.srcJs, ['js-transpile']);
+  gulp.watch(config.paths.srcHtml, ['htmlmin']);
   gulp.watch('src/scss/frontend/global/_variables.scss', ['variables']);
   gulp.watch('README.md', ['copy-readme', 'kss']);
 });
