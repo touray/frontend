a2107cc47d0529ae1a0f74e0ec8cd74f41c5005c
Ben Marshall <bmarshall511@gmail.com>
2017-09-06 15:13:27 -0500

    Created an example Drupal behavior.

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,11 @@
   $(document).foundation();
 })( jQuery );
 
+// Are you using Drupal 7? Make sure you're using behaviors!
+// @see https://www.lullabot.com/articles/understanding-javascript-behaviors-in-drupal
+//
+// Drupal.behaviors.exampleModule = {
+//   attach: (context, settings) => {
+//
+//   }
+// };
